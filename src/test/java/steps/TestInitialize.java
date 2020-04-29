63516f7025d57591f5a180729e0c131800652a7c
Andrejs Dudovics <adudovics@evolutiongaming.com>
2020-04-30 01:53:34 +0300

    Test set up is empty now, however left it here for future needs

--- src/test/java/steps/TestInitialize.java
+++ src/test/java/steps/TestInitialize.java
@@ -1,11 +1,14 @@
 package steps;
 
 import io.cucumber.java.Before;
+import lombok.extern.java.Log;
 
+@Log
 public class TestInitialize {
 
     @Before
     public void TestSetUp() {
+        log.info("Some tests setup if needed to implement in future");
     }
 
 }
